13f8af6abb03b522de05d4726e7ba86bcd594412
Giacomo Vacca <giacomo.vacca@gmail.com>
2013-10-25 18:33:41 +0100

    Improvements: 1. Prepend magic number to file at the beginning 2. Open (and close) file only once 3. Some documentation

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ silk_rtp_to_bitstream
 =====================
 
 Converts a SILK RTP stream into a SILK binary file for decoding
+
+BUILD
+
+gcc silk_rtp_to_bitstream.c -lpcap -o silk_rtp_to_bitstream
+
+
+USE
+
+./silk_rtp_to_bitstream <input pcap> <output .bit>
+
+The output .bit file can then be converted into PCM with SILK SDK, e.g.:
+
+./decoder silk.bit silk_PCM.raw
+
+Then convert into WAV with:
+
+sox -V -t raw -b 16 -e signed-integer -r 24000 silk_PCM.raw silk.wav
--- silk_rtp_to_bitstream.c
+++ silk_rtp_to_bitstream.c
@@ -1,14 +1,17 @@
 #include <pcap.h>
 #include <stdio.h>
 
+// 12 B RTP, 8 B UDP, 20 B IP, 14 B Ethernet, 2 B payload length
+#define HEADER_LEN 56
+
 void dispatcher_handler(u_char*, const struct pcap_pkthdr*, const u_char*);
 
 int main(int argc, char **argv) {
 	pcap_t* descr;
 	char errbuf[PCAP_ERRBUF_SIZE];
 
-	if (argc != 2) {
-		printf("Usage error.\n");
+	if (argc != 3) {
+		printf("Usage error. ./silk_rtp_to_bitstream <input pcap> <output .bit>\n");
 		return -1;
 	}
 
@@ -17,17 +20,7 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	pcap_loop(descr, 0, dispatcher_handler, NULL);
-
-	return 0;
-}
-
-
-void dispatcher_handler(u_char* temp1, const struct pcap_pkthdr* header, const u_char* pkt_data) {
-	u_int i = 0;
-
-	// TODO: pass output bit file name as argument
-	const char *bitOutFileName = "silk.bit"; // SILK bitstream
+	const char *bitOutFileName = argv[2]; // SILK bitstream for output
 	FILE* bitOutFile = fopen(bitOutFileName, "ab"); // append to binary file
 	if (bitOutFile == NULL) {
 		printf("Error: could not open output file %s\n", bitOutFileName);
@@ -37,10 +30,28 @@ void dispatcher_handler(u_char* temp1, const struct pcap_pkthdr* header, const u
 		printf("Output file %s opened successfully\n", bitOutFileName);
 	}
 
+	// Insert the magic number
+	// 23 21 53 49 4c 4b 5f 56  33
+        long magic_number_length = 9;
+	char magic_number[] = "#!SILK_V3";
+	fwrite(magic_number, sizeof(char), magic_number_length, bitOutFile);
+
+	pcap_loop(descr, 0, dispatcher_handler, (u_char*)bitOutFile);
+
+	fclose(bitOutFile);
+	return 0;
+}
+
+
+void dispatcher_handler(u_char* args, const struct pcap_pkthdr* header, const u_char* pkt_data) {
+	u_int i = 0;
+
+	FILE* bitOutFile = (FILE *)args;
+
 	// packet timestamp
 	printf("%ld:%ld ", header->ts.tv_sec, header->ts.tv_usec);
 
-	long payload_len = header->len - 56;
+	long payload_len = header->len - HEADER_LEN;
 
 	// Payload length
 	printf("Length: %ld\n", payload_len);
@@ -49,8 +60,8 @@ void dispatcher_handler(u_char* temp1, const struct pcap_pkthdr* header, const u
 	printf("%.4x ", payload_len);
 	fwrite(&payload_len, sizeof(char), 2, bitOutFile);
 
+	// print payload
 	fwrite(&pkt_data[56], sizeof(char), payload_len, bitOutFile);
 
-	fclose(bitOutFile);
 	printf("\n\n");
 }
